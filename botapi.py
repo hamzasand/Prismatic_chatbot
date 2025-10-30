bf9905fc29c1cf3e0760a87852add66240cade8c
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2025-10-30 11:48:11 +0500

    Update botapi.py

--- botapi.py
+++ botapi.py
@@ -1,11 +1,13 @@
 # import required libraries
 import os
 import re
-import pickle
+import time
 import faiss
+import pickle
+
 import numpy as np
 import smtplib
-import time
+
 
 import threading
 from email.mime.text import MIMEText
